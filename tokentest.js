037ffa08f01ddd0dd103e0a30fd0b129d294633e
sl-li15 <43902215+sl-li15@users.noreply.github.com>
2020-11-26 17:29:37 +0800

    Update tokentest.js

--- tokentest.js
+++ tokentest.js
@@ -2,7 +2,7 @@ import { Wechaty } from 'wechaty'
 import { ScanStatus } from 'wechaty-puppet'
 import QrcodeTerminal from 'qrcode-terminal';
 
-const token = 'puppet_donut_cfefa1bc9671df78'
+const token = 'YOUR_TOKEN_HERE'
 
 const bot = new Wechaty({
   puppet: 'wechaty-puppet-hostie',
